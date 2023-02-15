3544614878d515dbdf6d6dd3ce72de5df56dc0fa
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-02-15 23:55:14 +0600

    Create configuration

---
+++ api/api.js
@@ -0,0 +1,18 @@
+let APIConfig = {
+  ApiName: "JhUnJhuNiSmP Backend",
+  APIDescription: "Backend API of JhunJhuniSMP",
+  ServerConfig: {
+    PORT: 3000,
+    HostName: "localhost",
+  },
+  LoggerConfig: {
+    hostNameColor: "#4dff94",
+    mainColor: "#fc4e03",
+    portColor: "#08f7ff",
+    httpColor: "#7308ff",
+    startedColor: "#08ff56",
+    loggerText: "Server started on http://{hostname}:{port}",
+  },
+};
+
+module.exports = APIConfig;
