ec8266a3262476caa9947bcddefd3b18dcddd8c2
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-02-15 23:08:48 +0600

    Create loading component

---
+++ client/src/components/loadingComponent.jsx
@@ -0,0 +1,13 @@
+let BGStyle = {
+  backgroundColor: "black",
+};
+
+function LoadingComponent() {
+  return (
+    <div className="loading-component" style={BGStyle}>
+      <h1 color="white">Loading...</h1>
+    </div>
+  );
+}
+
+export default LoadingComponent;
