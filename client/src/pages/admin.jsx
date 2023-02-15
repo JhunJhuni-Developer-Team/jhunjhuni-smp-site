269a4051aecb389ec26423a8a352d02b5a2a50c7
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-02-15 23:09:35 +0600

    Create `admin` page

---
+++ client/src/pages/admin.jsx
@@ -0,0 +1,72 @@
+const AdminLogin = () => {
+  return (
+    <div className="AAAAAAA">
+      <div className="ADMIN">
+        <div className="admin-pg">
+          <div className="arekta-div">
+            <div className="ekhan theke shuru">
+              <div className="contents">
+                <div className="sesh hoi nai">
+                  <div className="vallage jalaite">
+                    <div className="wtf!">
+                      <div className="ekhono aso vai?">
+                        <div className="eto kulay kmne tmr?">
+                          <div className="infinite eida">
+                            <div className="next div e surprise">
+                              <div className="fuck you">
+                                <div className="🤏">
+                                  <div className="you have small">
+                                    <div className="dick">
+                                      <div className="no bitches?">
+                                        <div className="YO MF">
+                                          <div className="eije contents">
+                                            <div className="ekhono asos nice">
+                                              <div className="main">
+                                                <h1 className="your-mum">
+                                                  Admin Page
+                                                </h1>
+                                                <h2 className="fuck-yeah">
+                                                  Welcome to Admin page
+                                                </h2>
+                                                <form
+                                                  className="deeper-faster-yeah"
+                                                  action="/successful-admin-page-logged-in-no-jf-jej-775-STRONG!"
+                                                >
+                                                  <div className="I-love-you:3">
+                                                    <label
+                                                      htmlFor="pw"
+                                                      className="hey-I-wanna-fuck-you-bitch"
+                                                    ></label>
+                                                    <input
+                                                      id="pw"
+                                                      className="pw"
+                                                      name="pw"
+                                                    ></input>
+                                                  </div>
+                                                </form>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLogin;
